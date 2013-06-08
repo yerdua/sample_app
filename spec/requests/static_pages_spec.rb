a88aa552779dc8e150971d8a9277cbc89e5b7257
Audrey Penven <audrey.penven@gmail.com>
2013-06-08 16:12:51 -0700

    code cleanup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  let(:base_title) { @base_title = "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
     
-    it "has the right title" do
+    it "has the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "#{@base_title}")
     end
     
     it "has the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
+    end
+    
+    it "does not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
     
   end
@@ -26,7 +32,7 @@ describe "StaticPages" do
     it "has the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        :text => "#{@base_title} | Help")
     end
     
   end
@@ -42,6 +48,21 @@ describe "StaticPages" do
       visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+    
+  end
+  
+  describe "Contact page" do
+    
+    it "has the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+    
+    it "has the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "#{@base_title} | Contact Us")
     end
     
   end
